34fb2a2c6e8aff2e3bfc310a0084cf32567e6e42
Alex McCullough <alex.mccullough@capitalone.com>
2017-06-29 10:25:22 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './alerts/warning-alert/warning-alert.component';
+import {SuccessAlertComponent} from "./alerts/success-alert/success-alert.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
